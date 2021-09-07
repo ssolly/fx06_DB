c0bfe3682b495da6e5770296bcbb69a6faec04de
Choi.SY <sollly0829@gmail.com>
2021-09-07 09:36:44 +0900

    fx06.ex01 기본 틀 생성

---
+++ src/ex01/ex01.java
@@ -0,0 +1,20 @@
+package ex01;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ex01 extends Application{
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+		
+		arg0.show();
+		
+	}
+	
+	
+}
